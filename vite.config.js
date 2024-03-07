696311326c0e1be8c0f5ec87238c05f9aebf0ab1
MalazHassan <TheFoodanese@users.noreply.github.com>
2024-03-07 20:38:32 +0000

    src

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+  }
+});
+
+
+
+
